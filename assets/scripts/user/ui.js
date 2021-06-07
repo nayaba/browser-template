17f45675acab9888da862ad5058a35b4bb60d3cd
nayaba <nabila.ayaba@gmail.com>
2021-06-07 13:44:41 -0400

    Update all 'user' 'messaging' to appear in modal
    Customize the modal (also realize that I only need one, lol)

--- assets/scripts/user/ui.js
+++ assets/scripts/user/ui.js
@@ -2,48 +2,58 @@
 const store = require('./../store.js')
 
 const signUpSuccess = function (res) {
-  $('#messaging').text('Sign up successful')
+  $('#modal-01-body').html('Successfully signed up!')
+  $('#modal-01').modal('show')
   $('#sign-up-form').trigger('reset')
   $('#sign-in-form').trigger('reset')
   $('#sign-up-div').hide()
   $('#sign-in-div').show()
 }
 const signUpFail = function () {
-  $('#messaging').text('Sign up failed')
+  $('#modal-01-body').html('Sign up failed')
+  $('#modal-01').modal('show')
+  $('#sign-up-form').trigger('reset')
 }
 
 const signInSuccess = function (res) {
   $('#sign-in-div').hide()
   $('#nav').show()
   $('#after-sign-in').show()
-  $('#messaging').text('Sign in successful')
   store.user = res.user
   document.getElementById('entry-owner').value = store.user._id
   $('#sign-in-form').trigger('reset')
-  $('#sign-in-modal').modal('show')
+
+  $('#modal-01-body').html('Successfully signed in!')
+  $('#modal-01').modal('show')
 }
 const signInFail = function () {
-  $('#messaging').text('Sign in failed')
+  $('#modal-01-body').html('Sign in failed')
+  $('#modal-01').modal('show')
 }
 
 const changePasswordSuccess = function () {
-  $('#messaging').text('Successfully changed password')
+  $('#modal-01-body').html('Successfully changed password')
+  $('#modal-01').modal('show')
   $('#change-password-form').trigger('reset')
   $('#sign-in-form').trigger('reset')
 }
 const changePasswordFail = function () {
-  $('#messaging').text('Failed to change password')
+  $('#modal-01-body').html('Failed to change password')
+  $('#modal-01').modal('show')
+  $('#change-password-form').trigger('reset')
 }
 
 const signOutSuccess = function () {
-  $('#messaging').text('Successfully signed out')
+  $('#modal-01-body').html('Successfully signed out')
+  $('#modal-01').modal('show')
   $('#sign-up-form').trigger('reset')
   $('#sign-in-form').trigger('reset')
   $('#sign-in-div').show()
   $('#after-sign-in').hide()
 }
 const signOutFail = function () {
-  $('#messaging').text('Failed to sign out')
+  $('#modal-01-body').html('Failed to sign out')
+  $('#modal-01').modal('show')
 }
 
 const signUpLinkSuccess = function () {
