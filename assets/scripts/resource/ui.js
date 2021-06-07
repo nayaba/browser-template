4c1ffee28b82818de12dd7826b0615a58b3ca34f
nayaba <nabila.ayaba@gmail.com>
2021-06-07 15:47:54 -0400

    Place entries on cards
    Start the process of moving update entry to a modal
    Replace delete button with trashcan icon

--- assets/scripts/resource/ui.js
+++ assets/scripts/resource/ui.js
@@ -22,12 +22,14 @@ const indexEntriesSuccess = function (res) {
     <div class="row row-cols-1 row-cols-md-3 g-4">
     <div class="col">
       <div class="card" style="width: 18rem;">
-        <!-- <img src="..." class="card-img-top" alt="..."> -->
         <div class="card-body">
           <h5 class="card-title">${entry.symbol}</h5>
           <p class="card-text">${entry.text}</p>
           <div class="card-footer">
-          <button class="dynamic-delete-entry" data-id=${entry._id}>Delete Entry</button>
+          <button class="dynamic-delete-entry" data-id=${entry._id}><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+  <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
+  <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
+</svg></button>
           <form class="dynamic-update-entry" data-id=${entry._id}>
             <input class="update-entry-text" type="text" name="entry[text]" placeholder="text">
             <input class="update-entry-symbol" type="text" name="entry[symbol]" placeholder="symbol">
@@ -103,6 +105,11 @@ const navIndexEntries = function () {
   $('#index-entry-div').show()
 }
 
+const updateEntryModal = function () {
+  $('#modal-01-body').html('Failed to delete entry')
+  $('#modal-01').modal('show')
+}
+
 module.exports = {
   createEntrySuccess,
   createEntryFail,
@@ -115,5 +122,6 @@ module.exports = {
   deleteEntrySuccess,
   deleteEntryFail,
   navCreateEntry,
-  navIndexEntries
+  navIndexEntries,
+  updateEntryModal
 }
